0c7006811d99faab07ecc47b232bdd6a72d2d045
woojin126 <woojin126@naver.com>
2021-04-15 13:20:54 +0900

    스프링부트 애플리케이션

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan//스프링이자동으로 자기포함,하위패키지포함 다찾아서 서블릿등록해줌
 @SpringBootApplication
 public class ServletApplication {
 
